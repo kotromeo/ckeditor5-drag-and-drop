e5112d3827e398e4b12c6473d12ad9265b442801
Maxim Kazantsev <maxim.kazantsev@smartru.com>
2019-07-10 18:26:20 +0300

    click, drag events

--- src/drag-and-drop.js
+++ src/drag-and-drop.js
@@ -1,17 +1,36 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
 export default class DragAndDrop extends Plugin {
-	/**
-	 * @inheritDoc
-	 */
 	static get pluginName() {
 		return 'DragAndDrop';
 	}
-
-	/**
-	 * @inheritDoc
-	 */
+	constructor( editor ) {
+        super(arguments);
+        this.editor = editor;
+    }
 	init() {
-		console.log('DragAndDrop init');
+		let me = this,
+			editor = me.editor;
+		let selector = editor.config.get('dragId'),
+			elements = document.querySelectorAll(selector);
+		elements.forEach((element) => {
+		    element.onclick = (evt) => {
+		    	let target = evt.target;
+				evt.preventDefault();
+				editor.model.change( writer => {
+					const insertPosition = editor.model.document.selection.getFirstPosition();
+					writer.insertText(target.textContent, insertPosition );
+				});
+		    };
+		    element.ondragend = (evt) => {
+		    	let target = evt.target;
+				evt.preventDefault();
+				editor.model.change( writer => {
+					const insertPosition = editor.model.document.selection.getFirstPosition();
+					console.log('insertPosition ', insertPosition)
+					writer.insertText(target.textContent, insertPosition );
+				});
+		    };
+		});
 	}
 }
